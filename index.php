4b33d58ab38db9ea59aa2b1a6815632e4c8ebe76
AdeMola <ademolaomomeji@gmail.com>
2026-02-08 22:35:23 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
-﻿<?php
-session_start();
-
-?>
-
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
 <link href="images/favicon.png" rel="icon">
-<title>Oxyy - Register</title>
+<title>REGISTER FORM</title>
 <meta name="description" content="Login and Register Form Html Template">
 <meta name="author" content="harnishdesign.net">
 
@@ -24,11 +19,12 @@ session_start();
 <link rel="stylesheet" type="text/css" href="vendor/font-awesome/css/all.min.css">
 <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
 <!-- Colors Css -->
-<link id="color-switcher" type="text/css" rel="stylesheet" href="#">
+<link id="color-switcher" type="text/css" rel="stylesheet" href="css/color-orange.css">
 </head>
 <body>
 
 <!-- Preloader -->
+
 <div class="preloader">
   <div class="lds-ellipsis">
     <div></div>
@@ -37,116 +33,226 @@ session_start();
     <div></div>
   </div>
 </div>
+
 <!-- Preloader End -->
 
 <div id="main-wrapper" class="oxyy-login-register">
   <div class="container-fluid px-0">
-    <div class="row g-0 min-vh-100"> 
-      
-      <!-- Welcome Text
-      ========================= -->
-      <div class="col-md-6 col-lg-7 bg-light">
-        <div class="hero-wrap d-flex align-items-center h-100">
-          <div class="hero-mask opacity-8"></div>
-          <div class="hero-bg hero-bg-scroll" style="background-image:url('images/login-bg-7.jpg');"></div>
-          <div class="hero-content mx-auto w-100 h-100 d-flex flex-column">
-            <div class="container">
-              <div class="row g-0 mt-5">
-                <div class="col-11 col-md-10 col-lg-9 mx-auto">
-                  <h1 class="text-13 text-white fw-600 mb-4">To keep connected with largest shop in the world.</h1>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- Welcome Text End --> 
-      
-      <!-- Login Form
-      ========================= -->
+    <div class="row g-0 min-vh-100">
 
+      <!-- Register Form
+	  ========================= -->
 
-      <div class="col-md-6 col-lg-5 d-flex flex-column align-items-center">
-        <div class="container pt-4">
-          <div class="row g-0">
-            <div class="col-11 col-md-10 col-lg-9 mx-auto">
-              <div class="logo"> <a class="fw-600 text-6 text-dark" href="index.html" title="Oxyy">CREATE AN ACCOUNT</a> </div>
-            </div>
-          </div>
-        </div>
+    
+      <div class="col-md-6 col-lg-5 col-xl-4 d-flex flex-column bg-light shadow-lg order-2 order-md-1">
         <div class="container my-auto py-5">
           <div class="row g-0">
-            <div class="col-11 col-md-10 col-lg-9 mx-auto">
-              <h3 class="text-12 mb-4">REGISTER EMAIL</h3>
+            <div class="col-10 col-md-9 mx-auto text-center">
+              <div class="logo mb-4"> <a href="index.php" title="Oxyy">
+                <img src="images/logo-lg-orange.png" alt=""></a> 
+              </div>
 
-              <form id="loginForm" method="post">
+              <form id="signupForm" class="form-border" method="post" enctype= "multipart/form-data">
 
-              <?php
+                <?php
+              
+
               include("db_conn.php");
-              date_default_timezone_set("Africa/Lagos");
-              $OTP = rand(1000,9999);
+              date_default_timezone_set('Africa/Lagos');
+              $rand = rand(1000,9999);
+              $today = date("dmyhis");
+              $ID = "CSC". $today . $rand;
               error_reporting(E_ALL);
               if(isset($_REQUEST["submit"])){
+                $fullname = trim(addslashes($_REQUEST["fullname"]));
+                $username = trim(addslashes($_REQUEST["username"]));
                 $email = trim(addslashes($_REQUEST["email"]));
-                $otp = $_REQUEST["otp"];
+                $password = trim(addslashes($_REQUEST["password"]));
+                $state = $_REQUEST["state"];
+                $uin = $_REQUEST["uin"];
+                $agree = $_REQUEST["agree"];
+                $ipaddress = $_SERVER["REMOTE_ADDR"];
+                $device = $_SERVER["HTTP_USER_AGENT"];
 
-                $_SESSION["email"] = $email;
+                // FILE UPLOAD
+                $picture = $uin . $_FILES["picture"]['name'];
+                $target = "picture/";
+                $target = $target . $uin . $_FILES["picture"]['name'];
 
-                // CHECKING FOR DUPLICATE RECORD
-                $check = mysqli_query($conn, "SELECT * FROM otpver WHERE otp='$otp' OR email='$email'");
+                $cvUpload = $uin . $_FILES["cvupload"]['name'];
+                $cvTarget = "cvupload/";
+                $cvTarget = $cvTarget . $uin . $_FILES["cvupload"]['name'];
+
+                $certUpload = $uin . $_FILES["certupload"]['name'];
+                $certTarget = "certupload/";
+                $certTarget = $certTarget . $uin . $_FILES["certupload"]['name'];
+                // END
+
+                // CHECKING FOR DUPLICATE RECORDS
+                $check = mysqli_query($conn, "SELECT * FROM registerform WHERE uin = '$uin' OR email = '$email' OR username = '$username'");
                 $checkrows = mysqli_num_rows($check);
-
+                
                 if($checkrows > 0){
                   echo "<script>alert('Member already exists in database.')</script>";
                 }
                 else{
+                  // END
+                  
+                  // CHECKING FOR DUPLICATE FILE UPLOAD
+                if(move_uploaded_file($_FILES["picture"]['tmp_name'], $target) > 0){
+                if(move_uploaded_file($_FILES["cvupload"]['tmp_name'], $cvTarget) > 0) {
+                if(move_uploaded_file($_FILES["certupload"]['tmp_name'], $certTarget) > 0) {
+              //  END
 
-                $sql = "INSERT INTO otpver(email, otp, `status`) VALUES('$email', '$otp', 'Pending')";
-                mysqli_query($conn, $sql) or die(mysqli_error($conn));
-                $num = mysqli_insert_id($conn);
-                if(mysqli_affected_rows($conn)!= 1){
-                  $message = "Error inserting record into database.";
-                }
+                  
 
-                echo "<script>alert('OTP sent to $email');
-                window.location.href= 'otp.php'</script>";
-                
+            // INSERTING INTO TABLE(registerform) ON THE DATABASE.
+            // Password encryption using md5 or PASSWORD.
+            $sql = "INSERT INTO registerform (fullname, username, email, `password`, `state`, uin, agree, ipaddress, device, picture, cvupload, certupload) VALUES ('$fullname','$username','$email', md5('$password'),'$state','$uin', '$agree', '$ipaddress', '$device', '$picture', '$cvUpload', '$certUpload')";
+
+            mysqli_query($conn, $sql) or die(mysqli_error($conn));
+            $num = mysqli_insert_id($conn);
+            if(mysqli_affected_rows($conn)!=1){
+                $message = "Error inserting record into database";
+
+            }
+            // END
+
+            echo "<script>alert('Dear $fullname, your account has been successfully created. Thank you!')</script>";
               }
+
+              }
+
+              }
+
                 }
+
+              }
+
+              
 
               ?>
 
-
-                <label class="form-label fw-500" for="emailAddress">Email Address</label>
-                <div class="mb-3 icon-group icon-group-end">
-
-                  <input type="email" name="email" class="form-control bg-light border-light" id="emailAddress" required="" placeholder="Email or Username">
-                  <span class="icon-inside text-muted"><i class="fas fa-envelope"></i></span>
-				</div>
-
-                <div class="mb-3 icon-group icon-group-end">
-                  <input type="hidden" class="form-control form-control-lg bg-light border-light" id="loginPassword" required="" placeholder="Password" name= "otp" value="<?php echo "$OTP"; ?>">
-                  <span class="icon-inside text-muted"><i class="fas fa-lock"></i></span>
-				</div>
-               
-                <div class="d-grid my-4">
-                  <button class="btn btn-dark btn-lg" type="submit" name= "submit">Sign In</button>
+                  <div class="mb-3">
+                  <input type="text" class="form-control" id="fullName" required="" name="fullname" placeholder="Full Name">
                 </div>
-                <p class="text-2 text-muted text-center">Not a member? <a href="register-18.html">Sign Up now</a></p>
+
+
+                <div class="mb-3">
+                  <input type="text" class="form-control" id="username" name="username" required="" placeholder="Username">
+                </div>
+
+
+                <div class="mb-3">
+                  <input type="email" class="form-control" id="emailAddress" name="email" required="" placeholder="Email Address">
+                </div>
+
+
+                <div class="mb-3">
+                  
+                  <input type="password" class="form-control" id="loginPassword" name="password" required="" placeholder="Password">
+
+                </div>
+
+                <div class="mb-3">
+
+                  <select name="state" id="state" class="form-control" required="">
+                    <option value="">--Select State--</option>
+                    
+                    <?php
+                    
+                    $statelist = "Abia, Adamawa, Akwa-Ibom, Anambra, Bauchi, Bayelsa, Benue, Borno, Cross River, Delta, Ebonyi, Edo, Ekiti, Enugu, Gombe, Imo, Jigawa, Kaduna, Kano, Katsina, Kebbi, Kogi, Kwara, Lagos, Nasarawa, Niger, Ogun, Ondo, Osun, Oyo, Plateau, Rivers, Sokoto, Taraba, Yobe, Zamfara, Abuja";
+                    $arrStates = explode("," , $statelist);
+                    $countState = count($arrStates);
+                    $maincount = $countState - 1;
+
+                    for($x = 0; $x <= $maincount; $x = $x +1){
+                      echo "<option value= '$arrStates[$x]'>$arrStates[$x]</option>";
+                    }
+
+                    ?>
+
+                  </select>
+            
+                </div>
+
+                 <div class="mb-3">
+                  
+                  <input type="hidden" class="form-control" id="uin" name="uin" value= "<?php echo "$ID" ?>" required="" readonly>
+
+                </div>
+                <div class="mb-3">
+                  
+                  <input type="file" class="form-control" id="picture" accept= ".jpg,.jpeg,.png,.JPG,.PNG,.JPEG" name="picture" required="">
+
+                </div>
+
+                <div class="mb-3">
+                  
+                  <input type="file" class="form-control" id="cvupload" accept= ".pdf,.doc,.docx" name="cvupload" required="">
+
+                </div>
+
+                <div class="mb-3">
+                  
+                  <input type="file" class="form-control" id="certupload" accept= ".pdf,.doc,.docx,.PNG,.png,.JPG,.jpg" name="certupload" required="">
+
+                </div>
+
+                  <div class="form-check text-start my-4">
+                    <input id="agree" name="agree" class="form-check-input" type="checkbox" required="">
+                    <label class="form-check-label text-2" for="agree">I agree to the <a href="#">Terms</a> and <a href="#">Privacy Policy</a>.</label>
+                  </div>
+
+                <div class="d-grid my-4">
+					<button class="btn btn-primary text-uppercase" type="submit" name="submit" onclick= "return confirm(`Are you sure to create an account?`)">Create Account</button>
+				</div>
+
+        
+
+
               </form>
+
+              <p class="text-2">Already have an account? <a href="login-13.html">Sign In!</a></p>
             </div>
           </div>
         </div>
+        <div class="container py-2">
+          <p class="text-2 text-muted text-center mb-0">Copyright © <script>document.write(new Date().getFullYear())</script>  <a href="#"></a>. All Rights Reserved.</p>
+        </div>
       </div>
-      <!-- Login Form End --> 
       
+      <!-- Register Form End --> 
+      
+      <!-- Welcome Text
+      ========================= -->
+      
+      <div class="col-md-6 col-lg-7 col-xl-8 order-1 order-md-2">
+        <div class="hero-wrap h-100">
+          <div class="hero-bg hero-bg-scroll" style="background-image:url('images/login-bg.jpg');"></div>
+          <div class="hero-content w-100 h-100 d-flex flex-column">
+            <div class="row g-0 my-auto py-5">
+              <div class="col-10 col-lg-9 mx-auto text-center"> 
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Welcome Text End --> 
+      </div>
+
     </div>
   </div>
 </div>
 
+
+
+
 <!-- Script --> 
 <script src="vendor/jquery/jquery.min.js"></script> 
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script> 
+
 <!-- Style Switcher --> 
 <script src="js/switcher.min.js"></script> 
 <script src="js/theme.js"></script>
